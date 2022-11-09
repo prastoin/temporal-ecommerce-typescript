2a5f8fdfc115055ab8a21f193eb665d6293a3ae9
prastoin <tkesray@gmail.com>
2022-11-09 15:04:20 +0100

    feat: auto abandoned cart after given timeout

--- src/workflows.ts
+++ src/workflows.ts
@@ -19,8 +19,11 @@ export const removeFromCartSignal =
 // Queries
 export const getStateQuery = wf.defineQuery<WorkflowState>("getState");
 
+export const abandonedCartTimeoutMs = 3000;
+
 export async function cartWorkflow(initialProduct: Product): Promise<string> {
   console.log("CREATED WORKFLOW ", { initialProduct });
+  let abandonedCart = false;
   const state: WorkflowState = {
     productCollection: [initialProduct],
   };
@@ -42,7 +45,25 @@ export async function cartWorkflow(initialProduct: Product): Promise<string> {
     );
   });
 
-  await wf.condition(() => state.productCollection.length === 0);
+  wf.sleep(abandonedCartTimeoutMs)
+    .then(() => {
+      console.log("TIMED OUT ABANDONED CART");
+      abandonedCart = true;
+    })
+    .catch((e) => {
+      console.log("Abandoned cart timeout catch occurence");
+      console.error(e);
+    });
 
-  return "workflow_ended";
+  await wf.condition(
+    () => state.productCollection.length === 0 || abandonedCart
+  );
+
+  if (abandonedCart) {
+    console.log("Should be sending an email");
+    // TODO send email activity
+    return "abandoned_cart";
+  }
+
+  return "empty_cart";
 }
